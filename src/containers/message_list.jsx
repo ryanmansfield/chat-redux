9f2de7f3681bad951afd4e1339e58ffdb277991a
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-02-04 21:21:00 -0600

    fetchMessages action implemented

--- src/containers/message_list.jsx
+++ src/containers/message_list.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
+import { fetchMessages } from '../actions';
 import Message from '../components/message';
 
 class MessageList extends Component {
+  componentWillMount() {
+    this.fetchMessages();
+  }
+
+  fetchMessages = () => {
+    this.props.fetchMessages(this.props.selectedChannel);
+  }
+
   render () {
     return (
       <div className="channel-container">
@@ -29,5 +39,9 @@ function mapStateToProps (state) {
   };
 }
 
-export default connect(mapStateToProps)(MessageList);
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchMessages }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MessageList);
 
--- src/index.jsx
+++ src/index.jsx
@@ -17,18 +17,18 @@ import selectedChannelReducer from './reducers/selected_channel_reducer';
 const identityReducer = (state = null) => state;
 
 const initialState = {
-  messages: [
-    {
-      "author":"anonymous92",
-      "content":"Hello world!",
-      "created_at":"2017-09-26T23:03:16.365Z"
-    },
-    {
-      "author":"anonymous77",
-      "content":"My name is anonymous77",
-      "created_at":"2017-09-26T23:03:21.194Z"
-    }
-  ],
+  messages: [],
+  //   {
+  //     "author":"anonymous92",
+  //     "content":"Hello world!",
+  //     "created_at":"2017-09-26T23:03:16.365Z"
+  //   },
+  //   {
+  //     "author":"anonymous77",
+  //     "content":"My name is anonymous77",
+  //     "created_at":"2017-09-26T23:03:21.194Z"
+  //   }
+  // ],
   channels: ['general', 'react', 'chicago'],
   currentUser: prompt('What is your username?') || `anonymous${Math.floor(10+ (Math.random() * 90))}`,
   selectedChannel:'general'
